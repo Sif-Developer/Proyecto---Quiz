5806710268515ddc7068823b8651b97499d528b2
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-10-15 12:42:10 +0200

    Campio de orden, nombres y declaración en js

---
+++ quiz.js
@@ -0,0 +1,17 @@
+let tiempoRestante = 100;
+let contadorID;
+let contadorJS = document.getElementById("contador")
+let botonEmpezar = document.getElementById("boton-empezar")
+let botonSiguiente = document.getElementById("boton-siguiente")
+let contenedorPreguntas = document.getElementById("contenedor-preguntas")
+let contenedorEmpezar = document.getElementById("contenedor-empezar")
+let pregunta = document.getElementById("pregunta-titulo")
+let botonRespuesta = document.getElementById("boton-respuesta")
+let chequeaRespuesta = document.getElementById("chequea-respuesta")
+let mejoresResultados = document.getElementById("mejores-resultados")
+let botonEnviarNombre = document.getElementById("boton-enviar-nombre")
+let botonLimpiarPuntuacion = document.getElementById("boton-limpiar-puntuacion")
+let campoEscribeNombre = document.getElementById("nombre-jugador")
+let botonReiniciarQuiz = document.getElementById("boton-reiniciar")
+let campoPuntuacion = document.getElementById("tu-puntuacion")
+
