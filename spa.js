6cb4f1f1a5d682d375a8c213cd347524275d94e3
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-10-15 23:20:10 +0200

    Fix bug

---
+++ spa.js
@@ -0,0 +1,35 @@
+console.log("hola")
+
+//? DECLARAMOS LOS CONST
+ const homeNav = document.querySelector("#homeNav");
+ const questionsNav = document.querySelector("#questionsNav");
+ const resultsNav = document.querySelector("#resultsNav");
+
+ const home = document.querySelector("#home");
+ const questionsElement = document.querySelector("#questions");
+ const results = document.querySelector("#results");
+ 
+
+// const hideViews = () => {
+//   results.classList.add("hide");
+//   home.classList.add("hide");
+//   questionsElement.classList.add("hide");
+//  };
+// const goQuestions = () => {
+//   hideViews();
+//   questionsElement.classList.remove("hide");
+// };
+
+// const goHome = () => {
+//   hideViews();
+//   home.classList.remove("hide");
+// };
+
+// const goResults = () => {
+//   hideViews();
+//   results.classList.remove("hide");
+// };
+
+// questionsNav.addEventListener("click", goQuestions);
+// homeNav.addEventListener("click", goHome);
+// resultsNav.addEventListener("click", goResults);
