529c2fa3c13c925a2d689c9865ec21de6e1515d6
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-10-15 20:46:30 +0200

    Botón next no funcionar, falta implementar localStorage

--- preguntas.js
+++ preguntas.js
@@ -1,120 +1,47 @@
-let preguntas = [
-    {
-      "category": "Entertainment: Video Games",
-      "type": "multiple",
-      "difficulty": "medium",
-      "question": "When was the original Star Wars: Battlefront II released?",
-      "correct_answer": "October 31, 2005",
-      "incorrect_answers": [
-        "December 18, 2004",
-        "November 21, 2006",
-        "September 9, 2007"
+var preguntas = [
+  { 
+      question: "How do you write 'Hello World' in an alert box?", 
+      answers: [
+          { text: "msg('Hello World')", correct: false },
+          { text: "alert('Hello World')" , correct: true },
+          { text: "prompt('Hello World')", correct: false },
+          { text: "alertBox('Hello World')", correct: false }
       ]
-    },
-    {
-      "category": "Entertainment: Music",
-      "type": "multiple",
-      "difficulty": "medium",
-      "question": "Who is the lead singer of Silverchair?",
-      "correct_answer": "Daniel Johns",
-      "incorrect_answers": [
-        "Ben Gillies",
-        "Chris Joannou",
-        "George Costanza"
+  },
+  { 
+      question: "Which of the following function of Array object calls a function for each element in the array?", 
+      answers: [
+          { text: "concat()", correct: false },
+          { text: "filter()", correct: false },
+          { text: "forEach()", correct: true },
+          { text: "split()", correct: false }
       ]
-    },
-    {
-      "category": "Science & Nature",
-      "type": "multiple",
-      "difficulty": "hard",
-      "question": "Which one of these is scientific term for &quot;Brain Freeze&quot;?",
-      "correct_answer": "Sphenopalatine Ganglioneuralgia",
-      "incorrect_answers": [
-        "Hyacinthoides Italica",
-        "Amaranthus Retroflexus",
-        "Amblyomma Americanum"
+  },
+  { 
+      question: "How to write an IF statement for executing some code if 'i' is NOT equal to 8?", 
+      answers: [
+          { text: "if (i != 8)", correct: true },
+          { text: "if i =! 8", correct: false },
+          { text: "if (i <> 8)", correct: false },
+          { text: "if (i !=== 8)", correct: false }
       ]
-    },
-    {
-      "category": "Entertainment: Music",
-      "type": "multiple",
-      "difficulty": "hard",
-      "question": "Which of these songs is not by Tatsuro Yamashita?",
-      "correct_answer": "Lucky Lady Feel So Good ",
-      "incorrect_answers": [
-        "Merry-Go Round",
-        "Let&#039;s Dance Baby",
-        "Love Talkin&#039;"
+  },
+  { 
+      question: "What is the correct way to write a JavaScript array?", 
+      answers: [
+          { text: "var vegetable = (0:'kale', 1:'lettuce', 2:'spinach')", correct: false },
+          { text: "var vegetable = ['kale', 'lettuce', 'spinach']", correct: true },
+          { text: "var vegetable = (kale, lettuce, spinach)", correct: false },
+          { text: "None of the above", correct: false }
       ]
-    },
-    {
-      "category": "History",
-      "type": "multiple",
-      "difficulty": "medium",
-      "question": "Against which country did the Dutch Republic fight the Eighty Years&#039; War?",
-      "correct_answer": "Spain",
-      "incorrect_answers": [
-        "France",
-        "England",
-        "Portugal"
+  },
+  { 
+      question: "How do you round the number 102.456, to the nearest integer?",
+      answers: [
+          { text: "Math.random(102.456)", correct: false },
+          { text: "Math.rnd(102.456)", correct: false },
+          { text: "round(102.456)", correct: false },
+          { text: "None of the above", correct: true }
       ]
-    },
-    {
-      "category": "History",
-      "type": "multiple",
-      "difficulty": "hard",
-      "question": "What was found in 1946 by two young shepards near a cave?",
-      "correct_answer": "Dead Sea Scrolls",
-      "incorrect_answers": [
-        "The Blackbeard Chest",
-        "Sheep",
-        "The First Oaxaca Cave Sleeper"
-      ]
-    },
-    {
-      "category": "Entertainment: Television",
-      "type": "boolean",
-      "difficulty": "easy",
-      "question": "In &quot;Star Trek&quot;, Klingons are aliens.",
-      "correct_answer": "True",
-      "incorrect_answers": [
-        "False"
-      ]
-    },
-    {
-      "category": "Sports",
-      "type": "multiple",
-      "difficulty": "hard",
-      "question": "What is the full name of the footballer &quot;Cristiano Ronaldo&quot;?",
-      "correct_answer": "Cristiano Ronaldo dos Santos Aveiro",
-      "incorrect_answers": [
-        "Cristiano Ronaldo los Santos Diego",
-        "Cristiano Armando Diego Ronaldo",
-        "Cristiano Luis Armando Ronaldo"
-      ]
-    },
-    {
-      "category": "Entertainment: Japanese Anime & Manga",
-      "type": "multiple",
-      "difficulty": "hard",
-      "question": "In the Overlord Anime who was Cocytus made by?",
-      "correct_answer": "Warrior Takemikazuchi",
-      "incorrect_answers": [
-        "Peroroncino",
-        "Ulbert Alain Odle",
-        "Bukubukuchagama"
-      ]
-    },
-    {
-      "category": "Entertainment: Video Games",
-      "type": "multiple",
-      "difficulty": "medium",
-      "question": "Which one of the first four titles of the &quot;Grand Theft Auto&quot; franchise started the series of iconic image grid cover arts?",
-      "correct_answer": "Grand Theft Auto III",
-      "incorrect_answers": [
-        "Grand Theft Auto",
-        "Grand Theft Auto II",
-        "Grand Theft Auto Vice City"
-      ]
-    }
-  ]
+  },
+];
