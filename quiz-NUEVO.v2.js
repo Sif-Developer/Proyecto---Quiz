8debb56fd0a35c5f4d492b7c421883ab75476f6c
sergiokano <sergiocano.design@gmail.com>
2022-10-16 10:26:23 +0200

    contador puntuacion a 0

--- quiz-NUEVO.v2.js
+++ quiz-NUEVO.v2.js
@@ -7,32 +7,45 @@ const notaElement = document.querySelector(".nota");
 const disableButtons = document.getElementsByClassName("button")
 let currentQuestionIndex;
 let nota = 0;
-const questions = [
-  {
-    question: "What is 2 + 2?",
-    answers: [
-      { text: "4", correct: true },
-      { text: "22", correct: false },
-    ],
-  },
-  {
-    question: "Is web development fun?",
-    answers: [
-      { text: "Kinda", correct: false },
-      { text: "YES!!!", correct: true },
-      { text: "Um no", correct: false },
-      { text: "IDK", correct: false },
-    ],
-  },
-  {
-    question: "What is 4 * 2?",
-    answers: [
-      { text: "6", correct: false },
-      { text: "8", correct: true },
-      { text: "Yes", correct: false },
-    ],
-  },
-];
+
+// https://the-trivia-api.com/api/questions
+
+// functionaxios
+//   .get(https://the-trivia-api.com/api/questions)
+//   .then(res=> {
+//     return res.json();
+//   })
+//   .then (loadedQuestions => {
+//     console.log(loadedQuestions.results)
+//   });
+
+
+ const questions = [
+   {
+     question: "What is 2 + 2?",
+     answers: [
+       { text: "4", correct: true },
+       { text: "22", correct: false },
+     ],
+   },
+   {
+     question: "Is web development fun?",
+     answers: [
+       { text: "Kinda", correct: false },
+       { text: "YES!!!", correct: true },
+       { text: "Um no", correct: false },
+       { text: "IDK", correct: false },
+     ],
+   },
+   {
+     question: "What is 4 * 2?",
+     answers: [
+       { text: "6", correct: false },
+       { text: "8", correct: true },
+       { text: "Yes", correct: false },
+     ],
+   },
+ ];
 
 function startGame() {
   startButton.classList.replace("buttonStyle", "hide");
